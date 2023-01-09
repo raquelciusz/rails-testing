e8fc6805af600e9c97c2ef54d9dae923725897d2
Raquel Ciusz <raquel.ciusz@yahoo.com>
2023-01-09 10:59:07 -0300

    ubuntu issues still runing

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
+require "application_system_test_case"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "visiting the index" do
+    visit root_url # "/"
+    assert_selector "h1", text: "Awesome Products"
+  end
 end
